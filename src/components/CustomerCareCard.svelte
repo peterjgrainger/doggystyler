8493f1f6bfce3bef4a9b2eff153d8d50cf0a9a12
Peter Grainger <peter.grainger@arup.com>
2020-12-13 16:00:31 +0000

    routing and customer care card

---
+++ src/components/CustomerCareCard.svelte
@@ -0,0 +1,312 @@
+
+<h1>Dog Grooming</h1>
+<h2>Customer Care Card</h2>
+
+<div class="content-container">
+  
+</div>
+
+<div class="content-container">
+  <form class="half">
+    <h2>Owners Information</h2> 
+    <div class="form-row">
+      <div><p>Owner:&nbsp;&nbsp;&nbsp;</p></div>
+      <div class="signature-line"/>
+    </div>
+    <div class="form-row">
+      <div><p>Address:&nbsp;</p></div>
+      <div class="signature-line"/>
+    </div>
+    <div class="form-row">
+      <div><p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p></div>
+      <div class="signature-line"/>
+    </div>
+  </form>
+  <form class="half">
+      <div class="form-row">
+        <div><p>&nbsp;</p></div>
+      </div>
+      <div class="form-row">
+        <div><p>Telephone:&nbsp;</p></div>
+        <div class="signature-line"/>
+      </div>
+      <div class="form-row">
+        <div><p>Vets&nbsp;Name:&nbsp;</p></div>
+        <div class="signature-line"/>
+      </div>
+      <div class="form-row">
+        <div><p>Vets&nbsp;Tel:&nbsp;</p></div>
+        <div class="signature-line"/>
+      </div>
+  </form>
+</div>
+
+
+
+<div class="content-container">
+    <div class="thirty-percent">
+      <h2>Pet Information</h2>
+      <div class="form-row">
+        <div><p>Name:&nbsp;</p></div>
+        <div class="signature-line"/>
+      </div>
+      <div class="form-row">
+        <div><p>Colour:&nbsp;</p></div>
+        <div class="signature-line"/>
+      </div>
+      <div class="form-row">
+        <div><p>Behaviour:&nbsp;</p></div>
+        <div class="signature-line"/>
+      </div>
+    </div>
+    <div class="thirty-percent">
+      <h2></h2>
+      <div class="form-row">
+        <div><p>Age:&nbsp;</p></div>
+        <div class="signature-line"/>
+      </div>
+      <div class="form-row">
+        <div><p>Breed:&nbsp;</p></div>
+        <div class="signature-line"/>
+      </div>
+      <div class="form-row">
+        <div><p>Sex:&nbsp;</p></div>
+        <div class="signature-line"/>
+      </div> 
+    </div>
+  <form class="thirty-percent">
+    <h2>Medical History</h2>
+    <div class="form-row">
+      <p>Allergies:&nbsp;</p>
+      <div class="signature-line"/>
+    </div>
+    <div class="form-row">
+      <p>Vaccinated:&nbsp;</p>
+      <div class="signature-line"/>
+    </div>
+    <div class="form-row">
+      <p>Spayed:&nbsp;</p>
+      <div class="signature-line"/>
+    </div>
+  </form>
+</div>
+
+<div class="content-container">
+  <p class="language">
+    In signing this card I understand the above named pet is left at my own risk. In an emergency if a vet is required during the grooming process that the
+    cost will be payable by me. I have read and understand and agree to these terms.
+  </p>
+</div>
+<div class="content-container">
+  <div class="signature">
+    <div class="signature-line">
+      <p>
+        Owner's Signature:
+      </p>
+    </div>
+    <div class="date-line">
+      <p>
+        Date:
+      </p>
+    </div>
+  </div>
+</div>
+
+
+
+<style>
+  * {
+  box-sizing: border-box;
+}
+
+body,
+html {
+  width: 100%;
+  height: 100%;
+  padding: 0;
+  margin: 0;
+  font-family: sans-serif;
+}
+h1 {
+  display: block;
+  font-size: 2em;
+  margin-block-start: 0em;
+  margin-block-end: 0em;
+  margin-inline-start: 0px;
+  margin-inline-end: 0px;
+  font-weight: bold;
+}
+
+div {
+  margin-block-start: auto;
+}
+
+h2 {
+  display: block;
+  font-size: 1.5em;
+  margin-block-start: 0em;
+  margin-block-end: 0em;
+  margin-inline-start: 0px;
+  margin-inline-end: 0px;
+  font-weight: bold;
+}
+
+p {
+  margin-block-end: 0em;
+  font-size: 0.9em;
+}
+
+.ach-header {
+  display: flex;
+  flex-wrap: wrap;
+  width: 100%;
+  align-items: center;
+}
+.yourCo-logo-container {
+  width: 50%;
+  display: flex;
+}
+.company-logo-container {
+  width: 50%;
+  display: flex;
+  justify-content: flex-end;
+}
+.yourCo-logo-container > img,
+.company-logo-container > img {
+  width: 75%;
+  height: 75%;
+}
+.document-title {
+  display: flex;
+  justify-content: center;
+  color: #00795b;
+}
+.document-subtitle {
+  display: flex;
+  justify-content: center;
+  color: #00795b;
+}
+.content-container {
+  display: flex;
+  flex-wrap: wrap;
+  align-items: flex-start;
+  margin: 10px 0;
+}
+
+.two-thirds {
+  flex: 0 1 60%;
+  min-height: 100%;
+  min-width: 60%;
+  flex-direction: column;
+}
+
+.one-third {
+  flex: 0 1 40%;
+  display: flex;
+  height: 100%;
+  max-width: 40%;
+  flex-wrap: wrap;
+  align-items: flex-start;
+}
+
+.thirty-percent {
+  flex: 0 1 30%;
+  display: flex;
+  height: 100%;
+  max-width: 30%;
+  flex-wrap: wrap;
+  align-items: flex-start;
+}
+
+.half {
+  flex: 0 1 50%;
+  display: flex;
+  height: 100%;
+  max-width: 50%;
+  flex-wrap: wrap;
+  align-items: flex-start;
+}
+
+.half-containers {
+  flex: 0 0 100%;
+  max-height: 50%;
+  min-height: 50%;
+  max-width: 100%;
+  background: #0BDA51;
+  border: 1px solid red;
+  padding: .5em;
+  overflow: hidden;
+}
+
+.language {
+  display: flex;
+  width: 95%;
+  margin: 0;
+}
+.page-break {
+  display: flex;
+  width: 97%;
+  background-color: #00795b;
+  color: #fff;
+  justify-content: center;
+  margin: 10px 0;
+}
+#account-type {
+  flex-grow: 0;
+}
+.form-section {
+  width: 97%;
+  display: flex;
+  flex-wrap: wrap;
+}
+.form-section input {
+  flex-grow: 1;
+}
+.form-section label {
+  padding: 0 10px;
+  font-size: 0.9em;
+}
+.form-row {
+  display: flex;
+  width: 98%;
+  margin: 2px 0;
+}
+.signature {
+  display: flex;
+  flex-wrap: nowrap;
+  width: 95%;
+}
+.signature p {
+  margin: 10px 0 0;
+}
+.signature-line {
+  justify-content: flex-start;
+  width: 70%;
+  border-bottom: 0.5px solid black;
+}
+.date-line {
+  justify-content: flex-start;
+  width: 30%;
+  border-bottom: 0.5px solid black;
+}
+.signature-comment {
+  width: 100%;
+  font-weight: lighter;
+  font-size: 0.7em;
+  font-style: italic;
+}
+.instructions {
+  display: flex;
+  justify-content: center;
+  width: 98%;
+  color: #00795b;
+  font-weight: 700;
+}
+.form-print {
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+</style>
---
+++ src/components/LandingPage.svelte
@@ -0,0 +1,307 @@
+<script>
+  import Dog from './Dog.svelte'
+</script>
+<main>
+<header class="bg-image">
+	
+  <div class="container">
+	<Dog/>
+    <h1>dog grooming resources</h1>
+
+  </div>
+</header>
+
+<section class="">
+  <div class="container">
+    <div class="col-3 text--center">
+      <img src="http://store.storeimages.cdn-apple.com/4044/as-images.apple.com/is/image/AppleInc/aos/published/images/H/D1/HD162/HD162?wid=800&hei=800&fmt=jpeg&qlt=95&op_sharpen=0&resMode=bicub&op_usm=0.5,0.5,0,0&iccEmbed=0&layer=comp&.v=1400749861094" alt="" class="details-img--ball"/>
+    </div>
+    <div class="col-7 details">
+      <h3>Free Dog Report Cards</h3>
+      <p>Customisable report cards you can print off and give to customers</p>
+			 <a href="#" class="btn btn-transparent">Make yours now!</a>
+    </div>
+  </div>
+</section>
+
+<section class="section--primary--alt">
+    <div class="col-12 text--center">
+    <h3>Want to be the first to access new content?</h3>
+		<iframe id="big-form" src="https://docs.google.com/forms/d/e/1FAIpQLSeJIkSpIBr12HdlfnimV-j8-uhAtYU_uoojIeJnmnM_uuaI8A/viewform?embedded=true" width="640" height="638" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+		<iframe id="small-form" src="https://docs.google.com/forms/d/e/1FAIpQLSeJIkSpIBr12HdlfnimV-j8-uhAtYU_uoojIeJnmnM_uuaI8A/viewform?embedded=true" width="350" height="638" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+  </div>
+</section>
+</main>
+
+<style>
+
+@import url("//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css");
+@import url(//fonts.googleapis.com/css?family=Open+Sans:400,700,800,300);
+* {
+  box-sizing: border-box;
+}
+
+main {
+  width: 100%;
+  height: 100%;
+  margin: 0;
+  padding: 0;
+}
+
+#small-form {
+	display: none
+}
+
+
+
+@media (max-width: 43.75em) {
+  #small-form {
+		display: inline;
+	}
+
+	#big-form {
+		display: none;
+	}
+	
+}
+
+a {
+  text-decoration: none;
+}
+a:hover {
+  text-decoration: none;
+}
+
+img {
+  max-width: 100%;
+}
+
+.btn {
+  display: inline-block;
+  margin: 1rem 0;
+  color: white;
+  font-weight: 500;
+  font-size: 1.3rem;
+  background: #007ece;
+  letter-spacing: .02em;
+  border: none;
+  border-radius: 5px;
+  padding: .8rem 1rem .9rem;
+  text-shadow: 0 1px rgba(0, 0, 0, 0.3);
+  box-shadow: 0 0 2px rgba(0, 0, 0, 0.2);
+}
+@media (max-width: 43.75em) {
+  .btn {
+    padding: .5rem .7rem .6rem;
+    font-size: 1rem;
+  }
+}
+.btn:hover {
+  background: #008ee8;
+  color: #fff;
+}
+.btn:focus, .btn:active, .btn:focus:active {
+  background: #006eb5;
+  border-color: #006eb5;
+  box-shadow: 0 2px 5px 0 rgba(0, 0, 0, 0.5) inset;
+}
+
+.container {
+  margin: 0 auto;
+  width: 90%;
+  max-width: 900px;
+}
+
+header {
+  color: white;
+  background: #007ece;
+  padding: 5rem 0;
+  text-align: center;
+  position: relative;
+  z-index: 1;
+  overflow: hidden;
+}
+@media (max-width: 43.75em) {
+  header {
+    padding: 2rem 0;
+  }
+}
+header h1 {
+  font-size: 3rem;
+  margin: 0 0 1rem;
+}
+@media (max-width: 43.75em) {
+  header h1 {
+    font-size: 2rem;
+  }
+}
+
+section {
+  background: #fff;
+  color: #1c262b;
+  padding: 3.5rem 0;
+}
+@media (max-width: 43.75em) {
+  section {
+    padding: 2rem 0;
+  }
+}
+
+section.section--primary--alt {
+  background: #27a4f4;
+  color: #fff;
+}
+
+section h3 {
+  text-align: center;
+  font-size: 2rem;
+  font-weight: 300;
+  margin: 0 0 1rem;
+}
+@media (max-width: 43.75em) {
+  section h3 {
+    font-size: 1.5rem;
+  }
+}
+
+section p {
+  font-size: 1.2rem;
+  font-weight: 300;
+}
+
+.col-7, .col-3 {
+  margin: 0 1.5%;
+  display: inline-block;
+  vertical-align: top;
+}
+
+.col-7 {
+  width: 64%;
+}
+@media (max-width: 43.75em) {
+  .col-7 {
+    width: 100%;
+    margin: 0;
+  }
+}
+
+.col-3 {
+  width: 29%;
+}
+@media (max-width: 43.75em) {
+  .col-3 {
+    width: 100%;
+    margin: 0;
+  }
+}
+
+.col-5 {
+  width: 30%;
+}
+@media (max-width: 34.375em) {
+  .col-5 {
+    width: 60%;
+    margin: 0;
+  }
+}
+
+.details {
+  text-align: left;
+}
+.details h3 {
+  font-size: 2rem;
+  text-align: left;
+}
+
+@media (max-width: 43.75em) {
+  .details-img--ball {
+    height: 200px;
+    width: auto;
+    margin: 0 auto;
+  }
+}
+
+
+@media (max-width: 43.75em) {
+  .features {
+    width: 100%;
+    margin: 0;
+    text-align: left;
+    border-bottom: 1px solid rgba(255, 255, 255, 0.2);
+  }
+}
+
+@media (max-width: 43.75em) {
+  .features i {
+    font-size: 1.5rem;
+    width: 2rem;
+    text-align: center;
+    margin: 0 0 1rem 0;
+    float: left;
+  }
+}
+
+@media (max-width: 43.75em) {
+  .features p {
+    margin-left: 3rem;
+  }
+}
+
+blockquote {
+  position: relative;
+  margin: 0;
+  padding: 0;
+  text-align: center;
+}
+blockquote:before {
+  display: inline-block;
+  color: #007ece;
+  font-size: 2rem;
+  content: '\201C';
+}
+
+@media (max-width: 43.75em) {
+  blockquote p {
+    font-size: 1.2rem;
+  }
+}
+
+@media (max-width: 43.75em) {
+  blockquote cite {
+    font-size: .8rem;
+  }
+}
+
+.text--center {
+  text-align: center;
+}
+
+.bg-image {
+  background: #007ece;
+  text-align: center;
+  position: relative;
+  z-index: 1;
+  overflow: hidden;
+}
+.bg-image:before {
+  content: '';
+  display: block;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  width: 100%;
+  height: 100%;
+  z-index: -1;
+  background: #333;
+  background-size: cover;
+  background-attachment: fixed;
+  -webkit-filter: blur(5px);
+          filter: blur(5px);
+  opacity: .8;
+  -webkit-transform: scale(1.1);
+          transform: scale(1.1);
+}
+
+</style>
